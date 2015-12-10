919039aea2215559357d63ed9c4e573d045d78ba
Tomas Petricek <tomas@tomasp.net>
2015-12-10 23:29:18 +0000

    Boo, delete space

--- advent.fsx
+++ advent.fsx
@@ -29,4 +29,4 @@ i 3|>Seq.map"^ v< >".IndexOf|>Seq.scan(fun[x,y;o]i->[o;x+i/3*(i-4),y+(i%2-1)*(i-
 (i 5).Split '\n'|>Seq.filter((+)" ">>Seq.pairwise>>Seq.fold(fun(d,v)(a,b)->d||a=b,if b=a+'\001'&&(Seq.exists((=)a)"pacx")then 1<<<31 else v+sign(1+"aeiou".IndexOf b))(false,0)>>fun(d,v)->v>2&&d)|>Seq.length
 
 // Day #5 (b)
-(i 5).Split '\n'|>Seq.filter(fun s->"  "+s|>Seq.windowed 3|>Seq.mapi(fun i [|a;b;c|]->s.IndexOf(string b+string c)<i-2,a=c)|>Seq.reduce(fun(a,b)(c,d)->a||c,b||d)|>fun(a,b)->a&&b)|>Seq.length
+(i 5).Split '\n'|>Seq.filter(fun s->"  "+s|>Seq.windowed 3|>Seq.mapi(fun i[|a;b;c|]->s.IndexOf(string b+string c)<i-2,a=c)|>Seq.reduce(fun(a,b)(c,d)->a||c,b||d)|>fun(a,b)->a&&b)|>Seq.length
